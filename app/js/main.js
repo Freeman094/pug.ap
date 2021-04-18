e8472063371470739166418e19963025953c2b31
Pavel9897 <p.grechishnikov98@mail.ru>
2021-04-18 23:37:51 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+// Импортируем jQuery
+// ../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+// ../../node_modules/popper.js/dist/umd/popper.js
+
+// import uikit
+//= https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit.min.js
+//= https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit-icons.min.js
+
+// Импортируем другие js-файлы
+//= my.js
